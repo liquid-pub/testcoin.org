39454c9f242b68aa6055c97df421c053875f54c2
Melvin Carvalho <melvincarvalho@gmail.com>
2021-03-27 01:29:54 +0100

    webmap structure and content

--- webmaps/index.mm
+++ webmaps/index.mm
@@ -1,6 +1,6 @@
 <map version="freeplane 1.8.0">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
-<node TEXT="testcoin" FOLDED="false" ID="ID_908760835" CREATED="1616672832459" MODIFIED="1616804931120" LINK="index.mm" STYLE="oval">
+<node TEXT="testcoin.org" FOLDED="false" ID="ID_908760835" CREATED="1616672832459" MODIFIED="1616804899117" STYLE="oval">
 <font SIZE="18"/>
 <hook NAME="MapStyle">
     <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
@@ -63,11 +63,26 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="4" RULE="ON_BRANCH_CREATION"/>
 <node TEXT="links" POSITION="left" ID="ID_200723922" CREATED="1616672887799" MODIFIED="1616672890639">
 <edge COLOR="#ff0000"/>
+<node TEXT="domain" ID="ID_1713939624" CREATED="1616793777001" MODIFIED="1616804843216" LINK="https://testcoin.org/"/>
 <node TEXT="tx" ID="ID_1170463871" CREATED="1616804845002" MODIFIED="1616804848157" LINK="https://blockstream.info/liquid/tx/ed9a107828fb81828acf9b9355d031fc1ff10e2b66ee3c229eac49512e884132?expand"/>
 <node TEXT="proof" ID="ID_1824857751" CREATED="1616804862521" MODIFIED="1616804866160" LINK="https://testcoin.org/.well-known/liquid-asset-proof-59fb8b06d94ee4829ddcc66cfe0820e339df552ce4616e5325af23ca28f37b6d"/>
+</node>
+<node TEXT="Follow Your Nose" POSITION="right" ID="ID_589110765" CREATED="1616793782677" MODIFIED="1616793789716">
+<edge COLOR="#0000ff"/>
+<node TEXT="domain home page" ID="ID_1561194012" CREATED="1616793790490" MODIFIED="1616793805023">
+<node TEXT="link to assets" ID="ID_97728876" CREATED="1616793805730" MODIFIED="1616793808789"/>
+</node>
+</node>
+<node TEXT="Descriptions" POSITION="right" ID="ID_193484510" CREATED="1616793810095" MODIFIED="1616793814198">
+<edge COLOR="#00ff00"/>
+<node TEXT="home page" ID="ID_1722364542" CREATED="1616793815186" MODIFIED="1616793818173"/>
+<node TEXT="assets" ID="ID_128786551" CREATED="1616793818834" MODIFIED="1616793820097"/>
+</node>
+<node TEXT="testcoin" POSITION="right" ID="ID_163394869" CREATED="1616804888270" MODIFIED="1616804908029" LINK="testcoin.mm">
+<edge COLOR="#ff00ff"/>
 </node>
 </node>
 </map>
